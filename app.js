a2b9ca1d57a91dc20e826d1f13f98a07f206bdb9
faebzz <fabian.schaper13@gmail.com>
2018-05-29 22:18:22 +0200

    initial app

---
+++ app.js
@@ -0,0 +1,8 @@
+console.log('Hello World')
+// Functions
+const addTodo = () => {
+  console.log("Add todo");
+}
+
+// Listeners
+document.getElementById("addButton").addEventListener("click", addTodo);
