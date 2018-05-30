73da289d100d6b9562f42991311e6572bf23cdeb
faebzz <fabian.schaper13@gmail.com>
2018-05-30 21:58:17 +0200

    ordered files for publish

--- app.js
+++ app.js
@@ -1,8 +1,49 @@
-console.log('Hello World')
-// Functions
-const addTodo = () => {
-  console.log("Add todo");
-}
+function main() {
+  function addListItemToDom(name) {
+    var item = document.createElement("LI");
+    var list = document.getElementById("list");
+    var textnode = document.createTextNode(name);
+    item.onclick = function() {
+      this.parentNode.removeChild(this);
+      saveItemsToStorage();
+    }
+    item.appendChild(textnode);
+    list.insertBefore(item, list.firstChild);
+  }
 
-// Listeners
-document.getElementById("addButton").addEventListener("click", addTodo);
+  function saveItemsToStorage() {
+    var items = [];
+    var listItem = document.getElementById("list").getElementsByTagName("li");
+    var newNums = [];
+    for (var i = listItem.length-1; i >= 0; i--) {
+      items.push(listItem[i].innerHTML);
+    }
+    if (typeof(chrome.storage) !== 'undefined') {
+    	chrome.storage.local.set({items: items}, function() { });
+    }
+  }
+
+  function loadItemsFromStorage() {
+    if (typeof(chrome.storage) !== 'undefined') {
+    	chrome.storage.local.get(['items'], function(result) {
+        if(typeof(result.items) !== 'undefined'){
+          result.items.forEach(function(item){
+            addListItemToDom(item);
+          });
+        }
+      });
+    }
+  }
+
+  document.getElementById("inputTodo").addEventListener("keyup", function(e) {
+    if (event.which == 13 || event.keyCode == 13) {
+      addListItemToDom(this.value);
+      this.value = '';
+      saveItemsToStorage();
+    }
+  });
+  loadItemsFromStorage();
+};
+document.addEventListener("DOMContentLoaded", function() {
+  main();
+});
